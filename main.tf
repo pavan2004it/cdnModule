3f4fa71db60984aa82f0d657645fd4ddd094e2a1
Pavan Kumar <pavan_2004it@icloud.com>
2023-08-17 10:56:17 +0530

    updated the timeout

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
 resource "aws_cloudfront_distribution" "products_api" {
   origin {
     domain_name              = var.alb_dns_name
+    connection_attempts = 3
+    connection_timeout = 10
     custom_origin_config {
       http_port              = 80
       https_port             = 443
       origin_protocol_policy = "https-only"
       origin_ssl_protocols   = ["TLSv1.2"]
+      origin_keepalive_timeout = 10
+      origin_read_timeout = 60
     }
     custom_header {
       name  = "X-Custom-Header"
